b8c6e728e068e1f2cacd3007e69d76f7885a0403
Bruk Teshome <brukteshome21@gmail.com>
2024-05-16 22:10:57 +0300

    Add a vote struct to the contract

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.8;
 
 contract VotingSystem {
-  
+  struct Vote {
+    address voteCreater;
+    string[] votedList;
+    uint timeDuration;
+    mapping(string => uint) numVoted;
+    address[] voterAddress;
+    bool isTimeDuration;
+    bool isVoted;
+  }
 }
