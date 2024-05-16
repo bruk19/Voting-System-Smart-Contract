7ef72ef26a279a4c032ea9eac9734024df6d1015
Bruk Teshome <brukteshome21@gmail.com>
2024-05-16 22:18:22 +0300

    Create vote system function to create a new voting system

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,22 +2,25 @@
 pragma solidity ^0.8.8;
 
 contract VotingSystem {
-  struct Vote {
-    address voteCreater;
-    string[] votedList;
-    uint timeDuration;
-    mapping(string => uint) numVoted;
-    address[] voterAddress;
-    bool isTimeDuration;
-    bool isVoted;
-  }
+    struct Vote {
+        address voteCreater;
+        string[] votedList;
+        uint timeDuration;
+        mapping(string => uint) numVoted;
+        address[] voterAddress;
+        bool isTimeDuration;
+        bool isVoted;
+    }
 
-  address public owner;
-  mapping(string =>Vote) public voteInfo;
-  string[] public createdVoteList;
+    address public owner;
+    mapping(string => Vote) public voteInfo;
+    string[] public createdVoteList;
 
-  constructor () {
-    owner = msg.sender;
-  }
+    constructor() {
+        owner = msg.sender;
+    }
 
+    function createVoteSystem(string memory _voteName, string[] memory _votedNameList, uint _timeDuration) public {
+      
+    }
 }
