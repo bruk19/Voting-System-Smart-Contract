6b1088a36734cf8e2c8e22b2e30e6ca572d11a90
Bruk Teshome <brukteshome21@gmail.com>
2024-05-20 15:24:12 +0300

    Add a sepolis RPC and Private Key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: "0.8.24",
   networks: {
     sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
       chainId: 11155111,
     }
   }
