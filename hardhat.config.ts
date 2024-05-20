542f5d088fc4207ad06fded7824e5da3c3743338
Bruk Teshome <brukteshome21@gmail.com>
2024-05-20 15:17:27 +0300

    Add a sepolia netwrok to config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.24",
-};
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+    }
+  }
+}
 
 export default config;
